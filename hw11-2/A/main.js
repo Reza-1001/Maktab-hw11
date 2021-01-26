cdb61e5fa50bfdaf50a314e834c8afe676382efc
Reza Ahmad Nezhad <resistance1271@gmail.com>
2021-01-26 12:37:54 +0330

    hw11-2 created

---
+++ hw11-2/A/main.js
@@ -0,0 +1,9 @@
+console.log(process.cwd())
+const fs = require('fs')
+fs.readFile("hw11-2/A/files/from.txt", 'utf8',(err,data)=>{
+    if (err) return console.log(err.message);
+    fs.writeFile("hw11-2/A/files/to.txt", `${data} *Copied by Async Method*`,err=>{
+        if (err) return console.log(err.message);
+      console.log("Done");
+    })
+})
