dc7ba96522baa67a09e74f5697544bfccbd58d5f
Reza Ahmad Nezhad <resistance1271@gmail.com>
2021-01-28 01:32:59 +0330

    Final change-OK

--- hw11-4/main.js
+++ hw11-4/main.js
@@ -1,90 +1,47 @@
 const fs = require("fs");
-// let names=SplitFileData("hw11-4/files/names.txt");
-// let numbers=SplitFileData("hw11-4/files/numbers.txt");
-// show(names, numbers);
-// function SplitFileData(path,){
-// let a=[];
-// let b={};
-// let array=[];
-// fs.readFile(path,"utf8",(err,data)=>{
-//     for (let i of data.split(/\r\n/)){
-//
-// });
-// return (array);
-// }
-
-// function show(names,numbers){
-// console.log(names);
-// console.log(numbers);
-// }
+//Readint names.txt and numbers.txt
 let names = fs.readFileSync("hw11-4/files/names.txt","utf8").split(/\r\n/);
 let numbers = fs.readFileSync("hw11-4/files/numbers.txt","utf8").split(/\r\n/);
+//split id and names
 for (let i in names){
     names[i]=names[i].split("-")
 }
+//split id and phoneNumbers
 for (let i in numbers){
    numbers[i]=numbers[i].split("-")
     
 }
-console.log(names);
-console.log(numbers);
+//add ids and names to info array
+let info=[];
+for (let i of names){
+        info.push({
+            id:i[0],
+            name:i[1],
+            phoneNumber:[],
+    })
+}
+//adding phoneNumbers to info array based on id
+for (let i of numbers){
+    for (let j in info){
+        if (i[0]===info[j].id)
+        info[j].phoneNumber.push(i[1]);
+}
+}
+//converting info array to String
+let myString="";
+for (let i of info){
+    if (i.phoneNumber.length==1){
+        myString+=`${i.name}'s phone number is${i.phoneNumber}\n`;
+        continue;
+    }
+    if (i.phoneNumber.length>1){
+        myString+=`${i.name}'s phone numbers are${i.phoneNumber}\n`;
+        continue;
+    }
+    if (i.phoneNumber.length==0){
+        myString+=`${i.name} hasn't any phone number\n`;
+        continue;
+    }
+}
+fs.writeFileSync("hw11-4/files/result.txt",myString)
 
-
-
-
-// function FileRead(path, key1, key2) {
-//   let data = 
-//   let data2 = [];
-//   let a;
-//   let b = {};
-//   for (let i of data) {
-//     a = i.split("-");
-
-//     data2.push({ [key1]: a[0].trim(), [key2]: a[1].trim() });
-//   }
-//   return data2;
-// }
-
-
-// let merged=[];
-// for (let i in names){
-//     for (let j in numbers){
-//         if (names[i].id==numbers[j].id){
-//              obj={
-//                 id:names[i].id,
-//                 name:names[i].name,
-//                 phoneNumber:function(){
-//                     return this.push(numbers[j].phoneNumber)
-//                 },
-//             }
-//             merged.push(obj);
-//         }
-//     }
-// }
-// console.log(merged);
-
-// function Merge(a, b,index){
-//     let obj={};
-// for (let i in a){
-//     for (let j in b){
-//         obj["id"]=a[i];
-//         obj["name"]=a;
-//         names[index][j]=b[j];
-//     }
-// }
-// return
-// }
-
-
-
-
-// [
-//     { id: '001', name: 'mohammad' },
-//     { id: '002', name: 'Ali' },
-//     { id: '003', name: 'Zahra' }
-//   ]
-//   [
-//     { id: '001', phoneNumber: '09111111111' },
-//     { id: '002', phoneNumber: '09122222222' },
-//     { id: '002', phoneNumber: '09333333333' }
-//   ]
